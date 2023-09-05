95afb4e447893ac4cdea1b7c1e773816bdb40b2b
plyusnina <plyusnina@live.ru>
2023-09-05 14:48:32 +0500

    color link fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
             <h3 className="footer__name">Учебный проект Яндекс.Практикум х BeatFilm</h3>
             <div className="footer__block">
                 <ul className="footer__links">
-                    <li><Link to="https://practicum.yandex.ru" className="link" target="_blank">Яндекс.Практикум</Link></li>
-                    <li><Link to="https://github.com" className="link" target="_blank">Github</Link></li>
+                    <li><Link to="https://practicum.yandex.ru" className="link footer__link" target="_blank">Яндекс.Практикум</Link></li>
+                    <li><Link to="https://github.com" className="link footer__link" target="_blank">Github</Link></li>
                 </ul>
                 <p className="footer__year">©2023</p>
             </div>
